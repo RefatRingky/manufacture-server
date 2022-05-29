1f2b0037580a029b38b94f1f0f172ffd955c87fa
Refat Tamanna Ringky <95683348+RefatRingky@users.noreply.github.com>
2022-05-29 21:07:52 +0600

    middleware create

--- index.js
+++ index.js
@@ -12,9 +12,7 @@ const corsConfig = {
     credentials: true,
 }
 
-app.use(express.json());
-app.use(cors(corsConfig))
-app.options('*', cors(corsConfig))
+
 
 
 function verifyJWT(req, res, next) {
